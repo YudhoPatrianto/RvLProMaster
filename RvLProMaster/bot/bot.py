7c48935a1d30e9901627ded28f759c20a1bae29e
YudhoPatrianto <kydh01123@gmail.com>
2025-03-08 15:18:11 +0700

    Methods: sendMessage: Change `reply_markup` into str
    Signed-off-by: YudhoPatrianto <kydh01123@gmail.com>

--- RvLProMaster/bot/bot.py
+++ RvLProMaster/bot/bot.py
@@ -67,7 +67,7 @@ class Bot:
                 'parse_mode': parse_mode,
                 'disable_notification': disable_notification,
                 'protect_content': protect_content,
-                'reply_markup': reply_markup,
+                'reply_markup': str(reply_markup),
                 'reply_to_message_id': reply_message
             }
             async with AsyncClient() as client:
